a3ae6c469e342d96d868f48ee50b5e3c46552d98
Bald Mike <baldmike312@gmail.com>
2019-04-23 14:00:07 -0500

    team bio buttons

--- resources/js/components/TeamComponent.vue
+++ resources/js/components/TeamComponent.vue
@@ -33,10 +33,10 @@
                                     <h6 class="category">Founder</h6>
                                     <p class="card-description textOnWhite">
                                         Sarah grew up in Pennsylvania with tons of pets, including pot-bellied pigs. When she moved to Chicago, she volunteered at an open-admission shelter</p>
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="sarahBio">...Read Full Bio</n-button>
                                         
                                         
-                                        <p style="display: none;">,  and decided she wanted to make a difference in the lives of animals that were in dire need of rescue. As a transfer team member, she assisted rescues in evaluating dogs for their programs. Dogs that she interacted with were often euthanized. Sarah grew a thick skin. When she met Roo at the same shelter, she had no idea the crazy turn her life would take in the next 6 months. Live Like Roo is the epitome of everything that she believes in and she is happy to carry on his legacy through this Foundation.
+                                        <p class="card-description textOnWhite"style="display: none;">,  and decided she wanted to make a difference in the lives of animals that were in dire need of rescue. As a transfer team member, she assisted rescues in evaluating dogs for their programs. Dogs that she interacted with were often euthanized. Sarah grew a thick skin. When she met Roo at the same shelter, she had no idea the crazy turn her life would take in the next 6 months. Live Like Roo is the epitome of everything that she believes in and she is happy to carry on his legacy through this Foundation.
                                     </p>
                                     
                                 </div>
@@ -71,7 +71,7 @@
                                     <h6 class="category">VP/Medical Case Manager</h6>
                                     <p class="card-description textOnWhite">
                                         Laura can’t remember a time when animals weren’t a part of her life and she wouldn’t have it any other way. Her little family currently consists of </p>
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="lauraBio">...Read Full Bio</n-button>
                                         
                                         <p style="display: none;">2 cats, Fozzie and Frieda, and 1 dog, Henri, who is the self-proclaimed Mayor of Lincoln Park, and foster brother extraordinaire! It doesn’t stop at three, however, as Laura serves as a foster family for Alive Rescue, and will often have an extra one or two hanging out, until they find their forever family. Laura also serves as a Barn Leader at Alive Rescue’s adoption center, the Little Barn, and is an Adoption Coordinator. When Laura isn’t with her furry family or volunteering with Alive Rescue, she is working at Integrative Pet Care, as a certified canine rehabilitation assistant. She thinks of her patients as family and loves to see the progress they make after surgeries and injuries. Last year, Laura lost two of her beloved dogs, Elmer and Herman, to cancer, within 3 months of each other. When asked to be on the board for Live Like Roo Foundation, she knew that there would be no better way to honor their lives, than to be able to support families both emotionally and financially, who were experiencing the effects of canine cancer, as she had last year. In whatever free time Laura has, which isn’t a lot, she enjoys Cubs games, hanging out with friends at a dog friendly patio, sipping on a good beer, and seeking out the best hiking spots, to hang with the pups!
                                     </p>
@@ -107,7 +107,7 @@
                                         Nicole, a Chicago native who works in Remote Operations for the BIG Ten Network, grew up with rescue dogs and cats. After graduating from college
                                         </p>
 
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="nicoleBio">...Read Full Bio</n-button>
                                         <p style="display: none;">in 2005, Nicole and her now husband Caddy adopted Mia, an 8-month-old Rottweiler Sheppard Mix from the Anti Cruelty Society. After adopting Mia, Nicole felt like she was always trying to find the courage to volunteer at a shelter, but consistently resorted to volunteering at fundraising and awareness events out of fear of what she would see in the shelters. Then in 2009, their family grew with the adoption of a 7-week-old shepherd mix, Ole. While trying to attend as many fundraisers for local rescue groups as possible, Nicole came across the social media craze that was baby Jane and from there found the Live Like Roo Foundation. Meeting Sarah and the wonderful people on the LLR team helped her grow the strength she needed to step into the rescue world. Nicole is now a Barn Leader Assistant at Alive Rescue’s Little Barn and tries to attend every LLR Chicagoland Rescue Intervention and Support Program shift that she can.
 
                                         “Sarah and the LLR Foundation work so hard to help so many people and their pets in desperate times of need. Being a part of this team is a true honor to me.” 
@@ -143,7 +143,7 @@
                                     <h6 class="category">City Event Planner</h6>
                                     <p class="card-description textOnWhite">
                                         Kathy has loved dogs her entire life. Although she did not get her first rescue dog, Aria, until she was 14, her childhood in Warsaw Poland was spent bringing </p>
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="kathyBio">...Read Full Bio</n-button>
 
                                         <p style="display: none;">home stray dogs and trying to help as many as she could. Kathy has been volunteering with local rescues and fostering dogs in Chicago since 2006.  She and her only foster fail, Bill the Peanut, love being part of the Chicago rescue community. Kathy is thrilled she gets to utilize her love of event planning and fundraising to help the Live Like Roo Foundation.
 
@@ -181,7 +181,7 @@
                                     <h6 class="category">Run Like Roo Director</h6>
                                     <p class="card-description textOnWhite">
                                         One of Sara’s first words was “doggie”. Coincidence? We think not. Sara has always had a passion for pups, especially the “Bully Breeds”. Her hunk, Rocco </p>
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="saraBio">...Read Full Bio</n-button>
 
                                         <p style="display: none;">(a Pittie), has shown her the love and companionship these dogs are truly known to give. Wanting to help change the minds of so many people regarding the breed, Sara started volunteering in animal rescue. Soon after starting to volunteer, she became a foster (fail) with her first foster ~ #BlancheTheBeagle. In 2018, the one thing that Sara feared, happened ~ Rocco had a Mast Cell Tumor. After working with LLR – Sara truly saw what it was like on the other side of rescue. The side where she needed to learn on Roo & friends for support. Sara is happy to say that Rocco had tumor removal and is now cancer free!
 
@@ -221,7 +221,7 @@
                                     <h6 class="category">Graphic Designer</h6>
                                     <p class="card-description textOnWhite">
                                         Aside from having incredible fashion sense and sporting the most spectacular hairstyles, Dawn has always had a soft spot for big dogs. After moving to Chicago 22 years ago, the first dog she </p>
-                                        <p>...Read Full Bio</p>
+                                        <n-button type="primary" @click.prevent.native="sarahBio">...Read Full Bio</n-button>
 
                                         <p style="display: none;">adopted was Mousse, a Labrador Retriever from the Anti Cruelty Society. She volunteered there for a bit but found that, as an introvert, she was not well-suited to work the adoption floor. It wasn’t until several years later when she adopted Daryl Blu, who was saved during a CRISP shift at CACC, that she started really getting introduced to the Chicago rescue community. She learned about Live Like Roo and went to a couple blanket tying events and, when asked to help with some graphics, finally found her place in the rescue community.
                                         When not pounding away at the computer, she’s posting pictures of Daryl on Instagram, running a half marathon, or dodging tourists on the bike path.
@@ -259,9 +259,9 @@
                                     <p class="card-description textOnWhite">
                                         
                                     After a lifetime of crossing the street just to pet other people’s dogs, Lindsay finally got her own dog, Low, in 2011.  He’d been at a municipal, high-kill shelter </p>
-                                        <p>...Read Full Bio</p>
+                                    <n-button type="primary" @click.prevent.native="sarahBio">...Read Full Bio</n-button>
 
-                                        <p style="display: none;">for more than three months and it’s hard to say who got luckier the day Low was adopted.  When Lindsay shifted gears professionally, she started volunteering with One Tail at a Time and realized that helping dogs get second chances was some of the most fulfilling work she’d ever done.  Lindsay lives for CRISP shifts and is elated to be part of Team Roo, helping animals and owners who would give anything for more time together.
+                                    <p style="display: none;">for more than three months and it’s hard to say who got luckier the day Low was adopted.  When Lindsay shifted gears professionally, she started volunteering with One Tail at a Time and realized that helping dogs get second chances was some of the most fulfilling work she’d ever done.  Lindsay lives for CRISP shifts and is elated to be part of Team Roo, helping animals and owners who would give anything for more time together.
                                     To pay for her late-night Facebook fundraiser donation habits, Lindsay trains English Language Arts teachers around the country.  When not volunteering with dogs, Lindsay can be found throwing parties for The Birthday Party Project, reading trashy novels, or renting sparkly dresses and making high-profile mischief with her sister.  Lindsay and her incredibly tolerant husband have two dogs: Low, the best boy ever, and Fancy, an elderly, she-devil Pomeranian who uses her remaining few teeth to bite those who love her most.
                                     </p>
                                 </div>
@@ -294,6 +294,11 @@
     data() {
       return {
         
+        }
+    },
+    methods: {
+        sarahBio() {
+            console.log("sarah pushed");
         }
     }
   }
