d2239d72ab0b06a65e508958a1fb34c00f731859
Bald Mike <baldmike312@gmail.com>
2019-04-22 20:58:47 -0500

    cappy

--- resources/js/components/CappyComponent.vue
+++ resources/js/components/CappyComponent.vue
@@ -7,15 +7,13 @@
         margin-top: 120px">
 
            
-                <div class="col-md-3" style="position: sticky; top: 100px;">
-                    <h5 class="card-title" style="background-color: #384666; color: white; opacity: .9; padding: 20px; text-align: center;">"The Cappy Fund" named for our dear friend who passed too soon Captain Cowpants. Cappy was a bright light and a therapy dog, who recently passed away from cancer, leaving many people absolutely crushed. 
+            <div class="col-md-3" style="position: sticky; top: 100px;">
+                <h5 class="card-title" style="background-color: #384666; color: white; opacity: .9; padding: 20px; text-align: center;">"The Cappy Fund" named for our dear friend who passed too soon Captain Cowpants. Cappy was a bright light and a therapy dog, who recently passed away from cancer, leaving many people absolutely crushed. 
 
-                    This Fund is specifically for rescue dogs. As you know, we always cover any animal that is in a shelter or foster situation 100%, so we are honored that we can be a small part of Cappy's Legacy by helping to sponsor animals and organizations that really could use our funding. 
+                This Fund is specifically for rescue dogs. As you know, we always cover any animal that is in a shelter or foster situation 100%, so we are honored that we can be a small part of Cappy's Legacy by helping to sponsor animals and organizations that really could use our funding. 
 
-                    We love you Melissa and Adam and we are so sorry for the loss of your buddy, but we are so honored to provide this to others!</h5>
-                </div>
+                We love you Melissa and Adam and we are so sorry for the loss of your buddy, but we are so honored to provide this to others!</h5>
             </div>
-            
         </div>
     </div>
 </template>
