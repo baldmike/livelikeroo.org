c76bc50dd877d1de8bcd100ebb9ffdae2176a27a
Bald Mike <baldmike312@gmail.com>
2019-04-25 13:42:54 -0500

    user and message policies added to nova

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Models\Donation' => 'App\Policies\DonationPolicy',
         'App\Models\CarePackage' => 'App\Policies\CarePackagePolicy',
+        'App\Models\Message' => 'App\Policies\MessagePolicy',
+        'App\Models\User' => 'App\Policies\UserPolicy',
     ];
 
     /**
