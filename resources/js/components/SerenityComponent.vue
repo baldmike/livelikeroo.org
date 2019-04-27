0edaa1131246824bd93d696ec6dbc4328e49fa5d
Bald Mike <baldmike312@gmail.com>
2019-04-27 16:47:09 -0500

    programs funds

--- resources/js/components/SerenityComponent.vue
+++ resources/js/components/SerenityComponent.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container-fluid">
         <div class="jumbotron" style="background-image: url('images/serenity.jpg');
-        background-size: 100%;
+        background-size: contain;
         background-repeat: no-repeat;
         height: 1380px;
         margin-top: 100px">
 
             <div class="row">
-                <div class="col-md-3 offset-md-9" style="position: relative; top: 300px;">
+                <div class="col-md-3 offset-md-9" style="position: relative; top: 100px;">
                     <h5 class="card-title" style="background-color: #3d3293; color: white; opacity: .9; padding: 20px; text-align: center; font-size; 1.4rem;">Serenity's Wish is a grant based fund, available to Live Like Roo applicants. Serenity's Wish is intended to assist pet parents through end of life care and decision making, for their pets. This assistance will be limited to hospice consults, palliative care, and humane euthanasia and cremation."</h5>
 
                     <h5 class="card-title" style="background-color: #3d3293; color: white; opacity: .9; padding: 20px; text-align: center; font-size; 1.4rem;">"To inquire about assistance from Serenity's Wish, please complete an application for financial assistance at www.livelikeroo.org, then email roofinancials@gmail.com with Serenity's Wish, and your pet's name, in the subject line </h5>
