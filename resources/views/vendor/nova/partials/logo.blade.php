1ac359e35f09b945fce1a474db63a4103704999a
Bald Mike <baldmike312@gmail.com>
2019-05-22 17:20:05 -0500

    logo name wrong in nova

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -1,1 +1,1 @@
-<img class="ml-auto mr-auto" src="/images/LLR_logo.png" alt="Live Like Roo Logo" height="60" width="60">
+<img class="ml-auto mr-auto" src="/images/llr_logo.png" alt="Live Like Roo Logo" height="60" width="60">
