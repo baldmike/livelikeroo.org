f5ebb2d4939e1c338454ea13afed3d21f09903fc
Bald Mike <baldmike312@gmail.com>
2019-05-23 20:35:32 +0000

    email Emails

--- resources/views/Emails/message.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# This is a message from {{ $message -> first_name . ' ' . $message -> last_name }}, at {{ $message -> created_at }}:
-
-{{ $message -> message }}
-
-{{ $message -> first_name . ' ' . $message -> last_name }}<br>
-{{ $message -> email }}<br>
-via: livelikeroo.org
-@endcomponent
