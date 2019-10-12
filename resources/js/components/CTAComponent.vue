1c473a5c53dd74140f2c2252bcf0991881ac0988
Bald Mike <baldmike312@gmail.com>
2019-10-12 17:49:30 -0500

    clean up CTA for mobile

--- resources/js/components/CTAComponent.vue
+++ resources/js/components/CTAComponent.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-8 col-12" style="margin: 10px;">
                 <h2 class="title">BUY SWAG, HELP ROO!</h2>
                 <h3 class="description">Just click the blue "Support Now" button and check out these awesome hoodies, just in time for that crisp air....  All proceeds go to Live Like Roo, so THANK YOU!</h3>
 
-                <a href="https://www.ilgive.com/organizations/live-like-roo-foundation" target="_blank"><h2 class="title" style="text-align: left;">Click to check out our #ILGIVE page!</h2></a>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 col-12" style="margin: 10px;">
                 <iframe class="fundraising-widget" width="220px" height="420px" data-id="5725c6" data-url-key="llrswag" src="https://www.customink.com/fundraising/widgets/llrswag" data-utm-campaign="campaign-dashboard-embed-v1"></iframe>
+            </div>
+
+            <div class="row" style="margin: 10px;">
+                <a href="https://www.ilgive.com/organizations/live-like-roo-foundation" target="_blank"><h2 class="title" style="text-align: left;">Click to check out our #ILGIVE page!</h2></a>
             </div>
         </div>
     </div>
