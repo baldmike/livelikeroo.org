b651a5ea28f8b41b6413e46d9173a59d4bc47895
Bald Mike <baldmike312@gmail.com>
2019-10-12 16:41:14 -0500

    ilgive link added

--- resources/js/components/CTAComponent.vue
+++ resources/js/components/CTAComponent.vue
@@ -4,6 +4,8 @@
             <div class="col-md-8">
                 <h2 class="title">BUY SWAG, HELP ROO!</h2>
                 <h3 class="description">Just click the blue "Support Now" button and check out these awesome hoodies, just in time for that crisp air....  All proceeds go to Live Like Roo, so THANK YOU!</h3>
+
+                <a href="https://www.ilgive.com/organizations/live-like-roo-foundation" target="_blank"><h2 class="title" style="text-align: left;">Click to check out our #ILGIVE page!</h2></a>
             </div>
             <div class="col-md-4">
                 <iframe class="fundraising-widget" width="220px" height="420px" data-id="5725c6" data-url-key="llrswag" src="https://www.customink.com/fundraising/widgets/llrswag" data-utm-campaign="campaign-dashboard-embed-v1"></iframe>
